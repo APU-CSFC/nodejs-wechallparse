d1520e761a6609ffba9f3036c276165f898a947d
Maverick JS <mavjs01@gmail.com>
2014-04-18 00:41:41 +0800

    include long names of challenge sites

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('superagent');
 var cname = require('./chall.js');
+
 module.exports = function(username) {
     return new WeChall(username);
 }
@@ -26,7 +27,12 @@ function chall(msg) {
     var i = 0;
     var result = {};
     for(i; i<msg.length; i++) {
-        result[msg[i].split('(')[0]] = msg[i].split('(')[1].slice(0,-1);
+        var short_name = msg[i].split('(')[0];
+        var long_name = cname[short_name];
+        var percentage = msg[i].split('(')[1].slice(0,-1);
+        
+        result[short_name] = {};
+        result[short_name][long_name] = percentage;
     }
     return result;
 }
